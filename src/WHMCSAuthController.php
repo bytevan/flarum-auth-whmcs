06fe12a77ab6c3a30f09f1a2f73fde88ee45e41e
Jasonlong <londry520@gmail.com>
2018-11-01 21:46:59 +0800

    change namespace

--- src/WHMCSAuthController.php
+++ src/WHMCSAuthController.php
@@ -13,8 +13,8 @@ namespace Flarum\Auth\WHMCS;
 use Flarum\Forum\AuthenticationResponseFactory;
 use Flarum\Forum\Controller\AbstractOAuth2Controller;
 use Flarum\Settings\SettingsRepositoryInterface;
-use League\OAuth2\Client\Provider\WHMCS;
-use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use Londry\OAuth2\Client\Provider\WHMCS;
+use Londry\OAuth2\Client\Provider\ResourceOwnerInterface;
 
 class WHMCSAuthController extends AbstractOAuth2Controller
 {
